039a9f62404a40eaf68cf0403d7a12f63289f386
墨轩 <2564776749@qq.com>
2021-02-13 14:55:24 +0800

    登陆界面已编写好

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from "axios";
+import VueResource from "vue-resource"
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(VueResource);
+Vue.prototype.$http = axios;
+
+//配置请求的根路径
+axios.defaults.baseURL ="";
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-form :model="form" class="login_form">
+    <el-form-item>
+      <img src="../images/logo.jpg" alt="logo" class="logo">
+    </el-form-item>
+    <el-form-item class="input-group">
+      <el-input v-model="form.account" placeholder="请输入账号" type="text" show-word-limit minlength="6" maxlength="16"></el-input>
+    </el-form-item>
+    <el-form-item class="input-group">
+      <el-input show-password v-model="form.password" placeholder="请输入密码" type="password" minlength="6" maxlength="16"></el-input>
+    </el-form-item>
+    <el-form-item class="input-check">
+      <el-input type="text" placeholder="请输入验证码" v-model="form.check_mode" class="check_mode"></el-input>
+      <img src="" alt="点击生成验证码">
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="login(form)" class="login-button">登陆</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data(){
+    return{
+      form:{
+        account:"",
+        password:"",
+        check_mode:""
+      }
+    }
+  },
+  methods:{
+    login(){
+      //先进行长度校验，然后发送数据到服务器端进行数据验证，再由服务器返回判断结果，再根据结果进行回复或者跳转页面
+      //校验：中间再嵌套一个方法，比如check，当然，可以直接采用带校验的组件
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login_form{
+  width: 400px;
+  height: 500px;
+  margin-left: 500px;
+  border: 1px solid black;
+  border-radius: 5px;
+  background-clip: padding-box;
+  background-color: aliceblue;
+  margin-top: 150px;
+}
+.logo{
+  margin-left: 150px;
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  border-radius: 50%;
+}
+.check_mode{
+  width: 180px;
+}
+.login-button{
+  width: 100%;
+}
+</style>
