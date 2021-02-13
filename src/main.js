acb4968d6ca787cfb4affc2763ad05da85fb7235
墨轩 <2564776749@qq.com>
2021-02-13 14:55:24 +0800

    这是我第一次提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from "axios";
+import VueResource from "vue-resource"
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(VueResource)
+Vue.prototype.$http = axios
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
